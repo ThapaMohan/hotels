1a950b7ac0faadfbe05177577ff8d1a888b92146
ThapaMohan <mohanthapa1110@gmail.com>
2024-09-09 22:48:59 +0545

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,15 +1,20 @@
 const mongoose=require("mongoose");
+require('dotenv').config(); //importing .env file
 
 //Define the MongoDB connection URL
-const mongoURL="mongodb://localhost:27017/hotels";
+// const mongoURL="mongodb://localhost:27017/hotels";
 
+const mongoURL=process.env.MONGODB_URL;// access .env file
+
+// const mongoURL='mongodb+srv://hotel:Hotel123@hotels.ni4ea.mongodb.net/' // this atlas hosting
 //define parameters of mongoose and connections start
 // mongoose.connect(mongoURL,{
 //     useNewUrlParser:true,
 //     useUnifiedTopology:true,
 // });
-mongoose.connect('mongodb://localhost:27017/hotels');
+mongoose.connect(mongoURL);
 
+//mongose maintain the default connection with MongoDB
 const db =mongoose.connection;
 
 //defining events listener
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')// defining  express package for use
 const app = express();// blueprint of express or instances of express
 const db=require('./db');  // import db.js
+require('dotenv').config(); //importing .env file
 
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());// parse the json and store at req.body
+const PORT=process.env.PORT || 3000;// to access .env file we use process.env.var_name
 
 app.get('/', function (req, res) {
-  res.send('Hello! My name is mohan')
+  res.send('welcome to our hotel')
 });
 
 // app.post('/person',(req,res)=>{
@@ -21,6 +23,7 @@ app.use('/person',personRoutes);//use personroute
 const menuitemRoute=require('./routes/menuItemRoutes');
 app.use('/menu',menuitemRoute);
 
-app.listen(3000,()=>{
+
+app.listen(PORT,()=>{
   console.log("listening server");
 })//port number
