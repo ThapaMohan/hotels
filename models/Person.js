a9c4cede82e8fbbbd3fc32175f57ecd6f53ca7b2
ThapaMohan <mohanthapa1110@gmail.com>
2024-09-11 00:50:40 +0545

    authentication

--- models/Person.js
+++ models/Person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt=require('bcrypt');
 //defining schema
 const personSchema = new mongoose.Schema({
         name:{
@@ -28,9 +29,48 @@ const personSchema = new mongoose.Schema({
         salary:{
             type:Number,
             required:true
+        },
+        username:{
+            required:true,
+            type:String
+        },
+        password:{
+            required:true,
+            type:String
         }
 });
+personSchema.pre('save',async function (next){
+    const person=this;
 
+    //hash the password if only it modified or new
+    if(!person.isModified('password')) return next();
+    try{
+        //hash password generate
+        const salt= await bcrypt.genSalt(10);
+
+        //hash password
+        const hashedPassword=await bcrypt.hash(person.password,salt);
+
+        //override the plain password with hashed password
+        person.password=hashedPassword;
+
+        next();
+    }catch(err){
+        return next(err);
+    }
+    });
+
+    personSchema.methods.comparePassword=async function (candidatPassword) {
+        try{
+            //use bcrypt to compare provided password with hashed password
+            const isMatch=await bcrypt.compare(candidatPassword,this.password);
+            return isMatch;
+
+        }catch(err){
+            throw err;
+        }
+        
+    }
 //create models
 const Person = mongoose.model('Person', personSchema);
 
--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require('express')// defining  express package for use
 const app = express();// blueprint of express or instances of express
 const db=require('./db');  // import db.js
 require('dotenv').config(); //importing .env file
+const passport=require('./auth');//importing auth.js
 
-const bodyParser=require('body-parser');
+const bodyParser=require('body-parser');//importing body parser
 app.use(bodyParser.json());// parse the json and store at req.body
 const PORT=process.env.PORT || 3000;// to access .env file we use process.env.var_name
 
-app.get('/', function (req, res) {
+//middleware function
+const logRequest=(req,res,next) => {
+  console.log(`[${new Date().toLocaleString()}] Request made to: ${req.originalUrl}`);
+  next();//move to next phase
+}
+app.use(logRequest);// it define middleware for every routes
+
+app.use(passport.initialize());//to  use passport
+const localAuthMidleware=passport.authenticate('local',{session:false});//inseritng authentication 
+
+app.get('/',function (req, res) {//inserting authentication using local Strategy
   res.send('welcome to our hotel')
 });
 
@@ -15,12 +26,12 @@ app.get('/', function (req, res) {
 //     res.send("data is saved");
 // })
 
-//import personroute files
+//import personroute and menuitem route file
 const personRoutes=require('./routes/personRoutes');
-app.use('/person',personRoutes);//use personroute
-
-//import menuitem route file
 const menuitemRoute=require('./routes/menuItemRoutes');
+
+//use personroute and menuitem route
+app.use('/person',localAuthMidleware,personRoutes);
 app.use('/menu',menuitemRoute);
 
 
